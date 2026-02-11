ba71d6dc66851fd616b354f41c6c6bddb76b5c00
Konstantinos Platias <platiaskostas318@gmail.com>
2026-02-11 08:45:00 +0200

    Add analytics and speed insights for vercel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 function App() {
-  return <div>Your frontend is running my man, cogradulations!!!!</div>;
+  return (
+    <div>
+      Your frontend is running my man, cogradulations!!!!
+      <Analytics />
+      <SpeedInsights />
+    </div>
+  );
 }
 
 export default App;
