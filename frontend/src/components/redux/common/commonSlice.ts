013602aaa2ec95c833d56ea4e33b7d50e7ac4fa6
Konstantinos Platias <platiaskostas318@gmail.com>
2026-02-13 15:59:30 +0200

    Integrate redux to the frontend with test data

---
+++ frontend/src/components/redux/common/commonSlice.ts
@@ -0,0 +1,33 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { RootState } from "../../../stores/store";
+
+export interface CommonState {
+  value: string;
+  isActive: boolean;
+}
+
+export const initialState: CommonState = {
+  value: "",
+  isActive: false,
+};
+
+const commonSlice = createSlice({
+  name: "commonSlice",
+  initialState,
+  reducers: {
+    updateValue(state: CommonState, action: PayloadAction<string>) {
+      state.value = action.payload;
+    },
+
+    updateIsActive(state: CommonState, action: PayloadAction<boolean>) {
+      state.isActive = action.payload;
+    },
+  },
+});
+
+export const { updateValue, updateIsActive } = commonSlice.actions;
+
+export const selectValue = (state: RootState) => state.common.value;
+export const selectIsActive = (state: RootState) => state.common.isActive;
+
+export default commonSlice.reducer;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,21 @@
 import { useEffect } from "react";
 import { userService } from "../services/user.service";
+import { useAppDispatch, useAppSelector } from "../stores/hooks";
+import {
+  selectIsActive,
+  updateIsActive,
+} from "../components/redux/common/commonSlice";
+import { Button } from "@mui/material";
 
 export const HomePage = () => {
+  const dispatch = useAppDispatch();
+
+  const isActive = useAppSelector(selectIsActive);
+
+  const handleClick = () => {
+    dispatch(updateIsActive(!isActive));
+  };
+
   const fetchUsers = async () => {
     try {
       const data = await userService.getUsers();
@@ -17,6 +31,8 @@ export const HomePage = () => {
 
   return (
     <div>
+      {isActive && <div>Active is true</div>}
+      <Button onClick={handleClick}> Click me for active update </Button>
       <h1>Home Page</h1>
     </div>
   );
