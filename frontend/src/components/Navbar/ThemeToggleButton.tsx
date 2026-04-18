bcc306972f39cc137ee94a480fe6ae301177b321
Konstantinos Platias <platiaskostas318@gmail.com>
2026-04-18 14:32:56 +0300

    Small changes on translation

--- frontend/src/components/Navbar/ThemeToggleButton.tsx
+++ frontend/src/components/Navbar/ThemeToggleButton.tsx
@@ -1,14 +1,18 @@
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
 import { IconButton, Tooltip } from "@mui/material";
+import { useTranslation } from "react-i18next";
 
 import { useThemeMode } from "@/hooks/useThemeMode";
 
 export const ThemeToggleButton = () => {
+  const { t } = useTranslation();
   const { isDark, toggleTheme } = useThemeMode();
 
   return (
-    <Tooltip title={isDark ? "Light mode" : "Dark mode"}>
+    <Tooltip
+      title={isDark ? t("Navigation.lightMode") : t("Navigation.darkMode")}
+    >
       <IconButton onClick={toggleTheme} color="inherit">
         {isDark ? <LightModeOutlinedIcon /> : <DarkModeOutlinedIcon />}
       </IconButton>
